05a96f7ea959ce3ce9723f19b286503cc7557b25
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-04-03 18:28:46 +0200

    update statics

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+                    os.path.join(BASE_DIR, 'staticfiles')]
 
 DJANGO_SETTINGS_MODULE = 'oc_lettings_site.settings'
 
