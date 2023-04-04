6d32f9ee249d71a7e6389344b0b938637862bef6
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-04-04 15:47:26 +0200

    update statics

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -122,12 +122,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 sentry_sdk.init(
     dsn=env('SENTRY_DSN'),
